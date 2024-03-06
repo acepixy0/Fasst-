dbdaf18a1b43023bba45b6014973a1e540cd0cda
jomal <johnj18@farmingdale.edu>
2024-03-06 00:40:04 -0500

    - Firebase integration
    Added FirebaseDatabaseManager class for initializing the firebase connection to the app

--- Computer-Engineering-Capstone/MovieRatingService/src/main/java/module-info.java
+++ Computer-Engineering-Capstone/MovieRatingService/src/main/java/module-info.java
@@ -3,6 +3,9 @@ module com.example.movieratingservice {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires firebase.admin;
+    requires com.google.auth;
+    requires com.google.auth.oauth2;
 
     opens com.example.movieratingservice to javafx.fxml;
     exports com.example.movieratingservice;
